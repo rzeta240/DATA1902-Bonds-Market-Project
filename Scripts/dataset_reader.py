a4a816adf0cc26412b8e4206d4f9c20365a7fe35
Quezzert <quezzertx@gmail.com>
2025-09-04 13:28:57 +1000

    Yield Curve Loader and Visualisation added

---
+++ Scripts/dataset_reader.py
@@ -0,0 +1,78 @@
+import pandas as pd
+import numpy as np
+from datetime import datetime
+import os
+import math
+
+datasets_path = os.path.join(os.getcwd(), "Datasets")
+
+yield_curve_rates = pd.read_csv("Datasets/yield_curve_rates_daily.csv")
+
+def date(y, m, d):
+    return datetime.date(datetime(y, m, d))
+
+def get_yield_curve_rates():
+    df = pd.read_csv(os.path.join(datasets_path, "yield_curve_rates_daily.csv"))
+
+    for i in range( len( df["Date"] ) ):
+        date_str = df["Date"][i]
+
+        date_obj = datetime.date(datetime.strptime(date_str, "%m/%d/%Y"))
+        
+        df.at[i, "Date"] = date_obj
+
+    df = df[df["Date"] >= date(2013, 1, 1)]
+
+    # df.rename(columns={"1 Mo": 1, 
+    #                    "2 Mo": 2,
+    #                    "3 Mo": 3,
+    #                    "4 Mo": 4,
+    #                    "6 Mo": 6,
+    #                    "1 Yr": 12,
+    #                    "2 Yr": 24,
+    #                    "3 Yr": 36,
+    #                    "5 Yr": 60,
+    #                    "7 Yr": 84,
+    #                    "10 Yr": 120,
+    #                    "20 Yr": 240,
+    #                    "30 Yr": 360
+    #                    })
+    
+    percent_data_present = []
+
+    for i in range(len(df.columns)):
+        c = list(df.iloc[:, i])
+
+        for j in range( len( c ) ):
+            if type(c[j]) == float:
+                if np.isnan(c[j]):
+                    c[j] = None
+        
+        c_filtered = list(filter(None, c))
+
+        percent_data_present.append(f"{len(c_filtered) / len(c) * 100:.0f}%")
+
+    print("Yield Curve Data Loaded.\nPercent of data present:")
+    print(" | ".join([f"{df.columns[i]:^6}" for i in range( len( percent_data_present ) )]))
+    print(" | ".join([f"{percent_data_present[i]:^6}" for i in range( len( percent_data_present ) )]))
+
+    return df
+
+if __name__ == "__main__":
+    get_yield_curve_rates()
+
+# # Load the raw CSV and treat the "Date" column as dates not strings
+# df = pd.read_csv("yield-curve-rates-daily.csv", parse_dates=["Date"])
+
+# # Filter to 2013 onwards data and sort by date
+# df = df[df["Date"] >= "2013-01-01"].sort_values("Date")
+
+# # Reformat Date column to DD-MM-YYYY
+# df["Date"] = df["Date"].dt.strftime("%d-%m-%Y")
+
+# # Save cleaned data
+# df.to_csv("yield-curve-rates-2013-2024.csv", index=False)
+
+# # Print first & last 5 rows to confirm
+# print(df.head())
+# print(df.tail())
---
+++ yield_curve_visualisation.py
@@ -0,0 +1,48 @@
+import pandas as pd
+import numpy as np
+from datetime import datetime, timedelta
+import matplotlib.pyplot as plt
+from matplotlib.widgets import Slider
+
+from Scripts.dataset_reader import get_yield_curve_rates
+
+yield_curve_rates = get_yield_curve_rates()
+
+def curve_values(d):
+    row = yield_curve_rates.loc[yield_curve_rates["Date"] == d]
+
+    if not row.empty:
+        return row.iloc[:, 1:14].to_numpy().flatten()
+    else:
+        return np.array([])
+
+def date(y, m, d):
+    return datetime.date(datetime(y, m, d))
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+
+fig.subplots_adjust(left=0.1, bottom=0.25)
+
+m = np.array([1, 2, 3, 4, 6, 12, 24, 36, 60, 84, 120, 240, 360])
+date_0 = date(2013, 1, 2)
+
+[yield_curve] = ax.plot(m, curve_values(date_0), linewidth = 2, color="black")
+ax.set_xlim([0,360])
+ax.set_ylim([0, 10])
+
+date_slider_ax = fig.add_axes([0.15, 0.05, 0.65, 0.03], facecolor="grey")
+date_slider = Slider(date_slider_ax, "Date", 4015, 4380, valinit = 1)
+
+def on_slider_changed(val):
+    d = datetime(2013,1,1,0,0) + timedelta(days=round(val - 1))
+    d = datetime.date(d)
+
+    c = curve_values(d)
+    if len(c) > 1:
+        yield_curve.set_ydata(c)
+        fig.canvas.draw_idle()
+
+date_slider.on_changed(on_slider_changed)
+
+plt.show()
