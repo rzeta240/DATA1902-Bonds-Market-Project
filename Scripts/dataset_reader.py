0d5b1d96f4e58203d77dc8c63492ab53f30fd0eb
Ryan <quezzertx@gmail.com>
2025-09-06 09:24:45 +1000

    Major Push with too many changes to list

--- Scripts/dataset_reader.py
+++ Scripts/dataset_reader.py
@@ -5,12 +5,47 @@ import os
 
 datasets_path = os.path.join(os.getcwd(), "Datasets")
 
-yield_curve_rates = pd.read_csv("Datasets/yield_curve_rates_daily.csv")
-
 def date(y, m, d):
     return datetime.date(datetime(y, m, d))
 
-def get_yield_curve_rates():
+d_0 = date(2013, 1, 1)
+d_f = date(2024, 12, 31)
+
+def percent_present(data_name, df):
+    cols = df.columns
+
+    data_name_lens = [4] * len(cols)
+    for i in range( len( cols ) ):
+        data_name_lens[i] = max( len( cols[i] ), 4 )
+
+    percent_data_present = []
+
+    for i in range(len(df.columns)):
+        c = np.array(df.iloc[:, i])
+
+        filter = []
+
+        for j in range(len( c ) ):
+            filter.append(True)
+
+            if type( c[j] ) == str:
+                filter[j] = False
+                continue
+
+            if type( c[j] ) == np.float64:
+                if np.isnan( c[j] ):
+                    filter[j] = False
+
+        c_filtered = c[filter]
+
+        percent_data_present.append(f"{len(c_filtered) / len(c) * 100:.0f}%")
+
+    print(f"{data_name} Data Loaded.\nPercent of data present:")
+    print(" | ".join([f"{df.columns[i].center(data_name_lens[i])}" for i in range( len( percent_data_present ) )]))
+    print(" | ".join([f"{percent_data_present[i].center(data_name_lens[i])}" for i in range( len( percent_data_present ) )]))
+
+
+def get_yield_curve_rates(silent = False):
     df = pd.read_csv(os.path.join(datasets_path, "yield_curve_rates_daily.csv"))
 
     for i in range( len( df["Date"] ) ):
@@ -20,44 +55,103 @@ def get_yield_curve_rates():
         
         df.at[i, "Date"] = date_obj
 
-    df = df[df["Date"] >= date(2013, 1, 1)]
-
-    # df.rename(columns={"1 Mo": 1, 
-    #                    "2 Mo": 2,
-    #                    "3 Mo": 3,
-    #                    "4 Mo": 4,
-    #                    "6 Mo": 6,
-    #                    "1 Yr": 12,
-    #                    "2 Yr": 24,
-    #                    "3 Yr": 36,
-    #                    "5 Yr": 60,
-    #                    "7 Yr": 84,
-    #                    "10 Yr": 120,
-    #                    "20 Yr": 240,
-    #                    "30 Yr": 360
-    #                    })
+    df = df[df["Date"] >= d_0]
+    df = df[df["Date"] <= d_f]
     
-    percent_data_present = []
+    if not silent:
+        percent_present("Yield Curve", df)
 
-    for i in range(len(df.columns)):
-        c = list(df.iloc[:, i])
+    df.sort_values("Date")
 
-        for j in range( len( c ) ):
-            if type(c[j]) == float:
-                if np.isnan(c[j]):
-                    c[j] = None
+    return df
+
+def get_unemployment_rates(silent = False):
+    df = pd.read_csv(os.path.join(datasets_path, "unemployment_rate_monthly.csv"))
+
+    for i in range( len( df["Date"] ) ):
+        date_str = df["Date"][i]
+
+        date_obj = datetime.date(datetime.strptime(date_str, "%Y-%m-%d"))
         
-        c_filtered = list(filter(None, c))
+        df.at[i, "Date"] = date_obj
 
-        percent_data_present.append(f"{len(c_filtered) / len(c) * 100:.0f}%")
+    df = df[df["Date"] >= d_0]
+    df = df[df["Date"] <= d_f]
 
-    print("Yield Curve Data Loaded.\nPercent of data present:")
-    print(" | ".join([f"{df.columns[i]:^6}" for i in range( len( percent_data_present ) )]))
-    print(" | ".join([f"{percent_data_present[i]:^6}" for i in range( len( percent_data_present ) )]))
+    if not silent:
+        percent_present("Unemployment", df)
+
+    df.sort_values("Date")
+
+    return df
+
+def get_cpi(silent = False):
+    df = pd.read_csv(os.path.join(datasets_path, "consumer_price_index_quarterly.csv"))
+
+    for i in range( len( df["Date"] ) ):
+        date_str = df["Date"][i]
+
+        date_obj = datetime.date(datetime.strptime(date_str, "%m/%d/%Y"))
+        
+        df.at[i, "Date"] = date_obj
+
+    df = df[df["Date"] >= d_0]
+    df = df[df["Date"] <= d_f]
+
+    if not silent:
+        percent_present("CPI (Inflation)", df)
+
+    df.sort_values("Date")
+
+    return df
+
+def get_house_prices(silent = False):
+    df = pd.read_csv(os.path.join(datasets_path, "average_house_price_quarterly.csv"))
+
+    for i in range( len( df["Date"] ) ):
+        date_str = df["Date"][i]
+
+        date_obj = datetime.date(datetime.strptime(date_str, "%Y-%m-%d"))
+        
+        df.at[i, "Date"] = date_obj
+
+    df = df[df["Date"] >= d_0]
+    df = df[df["Date"] <= d_f]
+
+    if not silent:
+        percent_present("House Price", df)
 
     df.sort_values("Date")
 
     return df
 
 if __name__ == "__main__":
-    get_yield_curve_rates()
+    yield_rates = get_yield_curve_rates()
+    un_rates = get_unemployment_rates()
+    cpi = get_cpi()
+    house_prices = get_house_prices()
+
+    output_path = os.path.join(os.getcwd(), "Cleaned Data")
+
+    for i in range(len(yield_rates["Date"])):
+        yield_rates.at[i, "Date"] = yield_rates.loc[i, "Date"].strftime("%d/%m/%Y")
+    
+    yield_rates.pop("2 Mo")
+    yield_rates.pop("4 Mo")
+
+    yield_rates.to_csv(os.path.join(output_path, "yield_curve_rates_daily_2013_2024.csv"), index = False)
+
+    for i in range(len(un_rates["Date"])):
+        un_rates.at[i, "Date"] = un_rates.loc[i, "Date"].strftime("%d/%m/%Y")
+    
+    un_rates.to_csv(os.path.join(output_path, "unemployment_rate_monthly.csv"), index = False)
+
+    for i in range(len(cpi["Date"])):
+        cpi.at[i, "Date"] = cpi.loc[i, "Date"].strftime("%d/%m/%Y")
+    
+    cpi.to_csv(os.path.join(output_path, "consumer_price_index_quarterly.csv"), index = False)
+
+    for i in range(len(house_prices["Date"])):
+        house_prices.at[i, "Date"] = house_prices.loc[i, "Date"].strftime("%d/%m/%Y")
+    
+    house_prices.to_csv(os.path.join(output_path, "average_house_price_quarterly.csv"), index = False)
---
+++ yield_surface.py
@@ -0,0 +1,94 @@
+import pandas as pd
+import numpy as np
+from datetime import datetime, timedelta
+import matplotlib.pyplot as plt
+from matplotlib.widgets import Slider
+
+from matplotlib import cm
+from matplotlib.ticker import LinearLocator
+
+from Scripts.dataset_reader import get_yield_curve_rates
+
+yield_curve_rates = get_yield_curve_rates()
+
+def curve_values(d):
+    row = yield_curve_rates.loc[yield_curve_rates["Date"] == d]
+
+    if not row.empty:
+        return row.iloc[:, 1:14].to_numpy().flatten()
+    else:
+        return np.array([])
+
+def date(y, m, d):
+    return datetime.date(datetime(y, m, d))
+
+fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
+
+dates = yield_curve_rates[yield_curve_rates["Date"] >= date(2013, 1, 1)]["Date"]
+for i in range( len( dates ) ):
+    dates[i] = dates[i].toordinal() - date(2013, 1, 1).toordinal()
+
+t = np.array(sorted(dates))
+print(t)
+m = np.array([1, 2, 3, 4, 6, 12, 24, 36, 60, 84, 120, 240, 360])
+
+t, m = np.meshgrid(t, m)
+
+y = t.copy()
+
+# for i in range(len(t[0])):
+#     for j in range(len(t)):
+i = 0
+j = 0
+for i in range( len( t[0] ) ):
+    d = datetime(2013,1,1) + timedelta(days=round(t[0][i]))
+    d = datetime.date(d)
+
+    c = curve_values(d)
+
+    for j in range( len( t ) ):
+        try:
+            y[j][i] = c[j] * 100
+        except:
+            y[j][i] = 0
+            
+print(y)
+
+surf = ax.plot_surface(t, m, y, cmap=cm.coolwarm,
+                       linewidth=0, antialiased=False)
+
+# Customize the z axis.
+# ax.set_zlim(0, 0.5)
+
+# Add a color bar which maps values to colors.
+
+plt.show()
+
+# from stl import mesh
+
+# ny, nx = t.shape
+# vertices = np.column_stack([t.ravel(), m.ravel(), y.ravel()])
+
+# faces = []
+# for i in range(ny - 1):
+#     for j in range(nx - 1):
+#         # vertex indices for the 4 corners of the cell
+#         v0 = i * nx + j
+#         v1 = v0 + 1
+#         v2 = v0 + nx
+#         v3 = v2 + 1
+
+#         # two triangles: (v0, v2, v1) and (v1, v2, v3)
+#         faces.append([v0, v2, v1])
+#         faces.append([v1, v2, v3])
+
+# faces = np.array(faces)
+
+# surface_data = np.zeros(faces.shape[0], dtype=mesh.Mesh.dtype)
+# surface_mesh = mesh.Mesh(surface_data)
+
+# for i, f in enumerate(faces):
+#     for j in range(3):
+#         surface_mesh.vectors[i][j] = vertices[f[j], :]
+
+# surface_mesh.save('surface.stl')
