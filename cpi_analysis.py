afc2163b3ea85bbe4cc56389dff1fc60a1d648ca
Ryan <quezzertx@gmail.com>
2025-10-09 11:42:01 +1100

    Cpi analysis done

--- cpi_analysis.py
+++ cpi_analysis.py
@@ -16,4 +16,10 @@ iqr = q75 - q25
 
 outliers = df[ (df["CPIAUCSL_PC1"] > med + 1.5*iqr) | (df["CPIAUCSL_PC1"] < med - 1.5*iqr) ]
 
-print(outliers)
+print(q25, q75)
+print(q75 - q25)
+print(med)
+print(np.mean(df["CPIAUCSL_PC1"]))
+print(med - 1.5*iqr, med + 1.5 * iqr)
+for i in range(len(outliers["Date"])):
+    print(f"\\hline\n{outliers.iloc[i, 0].year} Q{(outliers.iloc[i, 0].month - 1)/3 + 1:.0f} & {outliers.iloc[i, 1]} \\\\")
