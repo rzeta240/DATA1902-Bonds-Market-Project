7f425301f7dd2e072496ab98cd33b2dafb0326cf
Vanessa Huang <vaness51139@gmail.com>
2025-10-11 13:28:05 +1100

    added coments and minor improvements

--- labor_heatmap.py
+++ labor_heatmap.py
@@ -15,22 +15,26 @@ filtered_df = filtered_df.set_index(df.iloc[:, 0])
 cleaned_sector_names = [col.replace('sector Labor productivity', '') for col in filtered_df.columns]
 filtered_df.columns = cleaned_sector_names
 
+# Convert dates to only years for y axis ticks
+years = [2013 + (i // 4) for i in range(len(filtered_df))]
+
 plt.figure(figsize=(10, 7.5))
 labor_heatmap = sns.heatmap(filtered_df, 
             cmap='RdYlGn', 
             center=0,
             cbar_kws={'label': '% Change in Productivity'},
             linewidths=0.5, 
-            linecolor='white')
+            linecolor='white', # Add separation between points for readability
+            yticklabels=years)
 
-plt.title('Labor Productivity by Sector Over Time')
+plt.title('Labor Productivity by Sector Over Time (2013-2024)')
 plt.ylabel('Year (Quarterly)')
 plt.xlabel('Sectors')
 plt.xticks(rotation=20)
 plt.tight_layout()
 
-for x in range(1, len(filtered_df.columns)):
-    plt.axvline(x, color='white', lw=1.2)
+# Add only one year tick for every four quarters for readability
+plt.yticks([i for i in range(0, len(filtered_df), 4)], labels=[years[i] for i in range(0, len(filtered_df), 4)])
+
 
 plt.show()
-
