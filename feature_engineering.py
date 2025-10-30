294cad63bcbc3c730b8ec4c2c5f7f8c9f1f13763
Ryan <quezzertx@gmail.com>
2025-10-30 21:15:33 +1100

    The push that fixes everything and makes our code 900% nicer

--- feature_engineering.py
+++ feature_engineering.py
@@ -24,10 +24,9 @@ data_full = pd.merge(data_full, labor_productivity, how = "outer", on = "Date")
 
 data_full.ffill(inplace=True)
 
+# output_path = os.path.join(os.getcwd(), "Cleaned Data")
 
-output_path = os.path.join(os.getcwd(), "Cleaned Data")
-
-data_full.to_csv("joined_file_filled.csv", index = False) # Save the dataset
+# data_full.to_csv("joined_file_filled.csv", index = False) # Save the dataset
 
 #Value that defines strength of the smoothing 
 alpha = 0.2 
@@ -36,13 +35,13 @@ for column in data_full.columns:
     if column != "Date":
         smoothed_col_name = f"{column}_smoothed"
         model = SimpleExpSmoothing(data_full[column])
-        data_full[smoothed_col_name] = (model.fit(smoothing_level = alpha, optimized = False)).fittedvalues
+        data_full = data_full.assign(**{str(smoothed_col_name): (model.fit(smoothing_level = alpha, optimized = False)).fittedvalues})
         column_name = f"{column}_resid"
-        data_full = data_full.assign(column_name = data_full[column] - data_full[smoothed_col_name])
+        data_full = data_full.assign(**{str(column_name): data_full[column] - data_full[smoothed_col_name]})
 
-output_path = os.path.join(os.getcwd(), "Cleaned Data")
+# output_path = os.path.join(os.getcwd(), "Cleaned Data")
 
-data_full.to_csv("joined_file_smoothing.csv", index = False) # Save the dataset
+# data_full.to_csv("joined_file_smoothing.csv", index = False) # Save the dataset
 
 look_fwd_windows = [3, 5, 7, 10, 15, 20, 25, 30, 40, 50, 60, 70, 80, 90, 100, 150, 200]
 spreads = [["1 Mo", "3 Mo"], ["1 Mo", "6 Mo"], ["3 Mo", "6 Mo"], ["3 Mo", "3 Yr"],["3 Mo", "1 Yr"], ["1 Yr", "3 Yr"], ["1 Yr", "5 Yr"], ["2 Yr", "10 Yr"], ["1 Yr", "10 Yr"], ["3 Yr", "5 Yr"], ["3 Yr", "20 Yr"], ["3 Yr", "30 Yr"], ["5 Yr", "10 Yr"], ["5 Yr", "20 Yr"], ["5 Yr", "30 Yr"], ["10 Yr", "30 Yr"], ["10 Yr", "20 Yr"]]
@@ -55,12 +54,41 @@ for column in yield_curve_data.columns:
     if column != "Date":
         for window in look_fwd_windows: 
             column_name = f"{column}_{window}d_change"
-            yield_curve_data[column_name] = yield_curve_data[column].shift(window) - yield_curve_data[column]
+            yield_curve_data = yield_curve_data.assign(**{column_name: yield_curve_data[column].shift(window) - yield_curve_data[column]})
 
-output_path = os.path.join(os.getcwd(), "Cleaned Data")
-output_path = os.path.join(os.getcwd(), "Cleaned Data")
+# output_path = os.path.join(os.getcwd(), "Cleaned Data")
 
-yield_curve_data.to_csv("yield_curve_ftreng.csv", index = False) # Save the dataset
-yield_curve_data.to_csv("yield_curve_ftreng.csv", index = False) # Save the dataset
+# yield_curve_data.to_csv("yield_curve_ftreng.csv", index = False) # Save the dataset
 
+data_full["Date"] = pd.to_datetime(data_full["Date"])
+data_full.set_index("Date", inplace=True)
+yield_curve_data["Date"] = pd.to_datetime(yield_curve_data["Date"])
+yield_curve_data.set_index("Date", inplace=True)
 
+all_dates = pd.date_range(start = "2013-01-01", end = "2024-12-31")
+
+filled_yield_data = yield_curve_data.reindex(all_dates, method='ffill')
+filled_yield_data = filled_yield_data.assign(Date = filled_yield_data.index)
+filled_yield_data.index = range(len(filled_yield_data.index))
+
+filled_yield_data.insert(0, 'Date', filled_yield_data.pop('Date'))
+
+data_full = data_full.assign(Date = data_full.index)
+data_full.index = range(len(data_full.index))
+
+data_full.insert(0, 'Date', data_full.pop('Date'))
+
+all_data = pd.merge(data_full, filled_yield_data, how = "left", on = "Date")
+
+train = all_data[all_data["Date"] < '2020-12-31']
+
+validation = all_data[(all_data["Date"] < '2022-12-31')&(all_data["Date"] > '2020-12-31')]
+
+x_cols = list(data_full.columns)
+y_cols = list(filled_yield_data.columns)
+
+train[x_cols].to_csv("training_x_data.csv", index = False)
+train[y_cols].to_csv("training_y_data.csv", index = False)
+
+validation[x_cols].to_csv("validation_x_data.csv", index = False)
+validation[y_cols].to_csv("validation_y_data.csv", index = False)
--- ridge_regression.py
+++ ridge_regression.py
@@ -1,60 +1,57 @@
 import pandas as pd 
 import datetime as dt
 import numpy as np
+import os
 from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.linear_model import ridge_regression, Ridge
 import matplotlib.pyplot as plt
 
+def load_data():
+    global x_train
+    global x_validation
+    global y_train
+    global y_validation
+    global y_cols
+    
+    x_train = pd.read_csv("training_x_data.csv")
+    x_train["Date"] = pd.to_datetime(x_train["Date"])
+    x_train.set_index("Date", inplace=True)
+    x_validation = pd.read_csv("validation_x_data.csv")
+    x_validation["Date"] = pd.to_datetime(x_validation["Date"])
+    x_validation.set_index("Date", inplace=True)
 
-eco_data = pd.read_csv("joined_file_smoothing.csv")
-yield_data = pd.read_csv("yield_curve_ftreng.csv")
+    y_train = pd.read_csv("training_y_data.csv")
+    y_train["Date"] = pd.to_datetime(y_train["Date"])
+    y_train.set_index("Date", inplace = True)
+    y_validation = pd.read_csv("validation_y_data.csv")
+    y_validation["Date"] = pd.to_datetime(y_validation["Date"])
+    y_validation.set_index("Date", inplace=True)
 
-eco_data["Date"] = pd.to_datetime(eco_data["Date"])
-eco_data.set_index("Date", inplace=True)
-yield_data["Date"] = pd.to_datetime(yield_data["Date"])
-yield_data.set_index("Date", inplace=True)
-yield_data["Date"] = pd.to_datetime(yield_data["Date"])
-yield_data.set_index("Date", inplace=True)
-y_cols = list(yield_data.columns)
-x_cols = list(eco_data.columns)
+    y_cols = y_train.columns
 
-all_dates = pd.date_range(start = "2013-01-01", end = "2024-12-31")
+try:
+    load_data()
+except:
+    os.system("python3 feature_engineering.py") 
+    load_data()
 
-#Forward fills any missing dates in YC data to ensure there are predictors for every Eco Data input (when the first of the month falls on a weekday)
-filled_yield_data = yield_data.reindex(all_dates, method='ffill')
-filled_yield_data = filled_yield_data.assign(Date = filled_yield_data.index)
-filled_yield_data.index = range(len(filled_yield_data.index))
-filled_yield_data = yield_data.reindex(all_dates, method='ffill')
-filled_yield_data = filled_yield_data.assign(Date = filled_yield_data.index)
-filled_yield_data.index = range(len(filled_yield_data.index))
-
-filled_yield_data.insert(0, 'Date', filled_yield_data.pop('Date'))
-
-all_data = pd.merge(eco_data, filled_yield_data, how = "left", on = "Date")
-
-#split into Training, validation, test - 8 yrs for training, 2 for validation, 2 for test
-train = all_data[all_data["Date"] < '2020-12-31']
-validation = all_data[(all_data["Date"] < '2022-12-31')&(all_data["Date"] > '2020-12-31')]
 #We want to loop through each y-variable and see which ones are most explainable by our input data. 
 results = []
 #Taking initial value of alpha = 10 (can change later...)
 #First, just training and evaluating on model fitted to training data - just to make our processes really rigorous
-x_train = train[x_cols]
-x_validate = validation[x_cols]
-alp = 10
+alp = float(10)
 for column in y_cols: 
-    y_train = train[column]
-    y_validate = validation[column]
+    y_train_col = y_train[column]
+    y_validation_col = y_validation[column]
     model = Ridge(alp)
-    model.fit(x_train, y_train)
-    y_pred = model.predict(x_validate)
-    mse = mean_squared_error(y_validate, y_pred)
-    rsq = r2_score(y_validate, y_pred)
+    model.fit(x_train, y_train_col)
+    y_pred = model.predict(x_validation)
+    mse = mean_squared_error(y_validation_col, y_pred)
+    rsq = r2_score(y_validation_col, y_pred)
     results.append({'y_column': column, 'MSE': mse, 'R_squared': rsq})
 
 # Create a DataFrame from the results
 results_df = pd.DataFrame(results)
-
 
 # Sort the results by R_squared in descending order
 sorted_results = results_df.sort_values(by='R_squared', ascending=False)
@@ -67,22 +64,20 @@ fine_tuned_results = []
 for column in selected_y_vals: 
     best_rsq = 0
     for alpha in alphas: 
-        y_train = train[column]
-        y_validate = validation[column]
+        y_train_col = y_train[column]
+        y_validation_col = y_validation[column]
         model = Ridge(alpha)
-        model.fit(x_train, y_train)
-        y_pred = model.predict(x_validate)
-        mse = mean_squared_error(y_validate, y_pred)
-        rsq = r2_score(y_validate, y_pred)
+        model.fit(x_train, y_train_col)
+        y_pred = model.predict(x_validation)
+        mse = mean_squared_error(y_validation_col, y_pred)
+        rsq = r2_score(y_validation_col, y_pred)
         if rsq > best_rsq: 
             best_rsq = rsq
             best_alpha = alpha
     fine_tuned_results.append({'y_column': column, 'Best_Alpha': best_alpha, 'R_squared': best_rsq})
 fine_tuned_results = pd.DataFrame(fine_tuned_results).sort_values(by='R_squared', ascending=False)
 
-
-
-selected_y_vals = list(fine_tuned_results[10:26]["y_column"])
+selected_y_vals = list(fine_tuned_results.iloc[:16, :]["y_column"])
 
 grid_size = 4
 
@@ -91,19 +86,20 @@ axes = axes.flatten()
 i = 0
 for column in selected_y_vals: 
     alp = fine_tuned_results.loc[fine_tuned_results["y_column"]==column, "Best_Alpha"]
-    y_train = train[column]
-    y_validate = validation[column]
-    model = Ridge(float(alp))
-    model.fit(x_train, y_train)
-    y_pred = model.predict(x_validate)
-    rsq = r2_score(y_validate, y_pred)
-    mse = mean_squared_error(y_validate, y_pred)
+    y_train_col = y_train[column]
+    y_validation_col = y_validation[column]
+    model = Ridge(alp.iloc[0])
+    model.fit(x_train, y_train_col)
+    y_pred = model.predict(x_validation)
+    rsq = r2_score(y_validation_col, y_pred)
+    mse = mean_squared_error(y_validation_col, y_pred)
     # Create a subplot for each target variable
-    axes[i].scatter(y_validate, y_pred, alpha=0.6)
-    axes[i].plot(y_validate, y_validate, color='red', linestyle='--')  # Line of equality
+    axes[i].scatter(y_validation_col, y_pred, alpha=0.6)
+    axes[i].plot(y_validation_col, y_validation_col, color='red', linestyle='--')  # Line of equality
     axes[i].set_title(f'{column} (R² = {rsq:.2f}), (MSE = {mse:.2f})')
     axes[i].set_xlabel('Actual Values')
     axes[i].set_ylabel('Predicted Values')
     i += 1
+    
 plt.tight_layout()
 plt.show()
